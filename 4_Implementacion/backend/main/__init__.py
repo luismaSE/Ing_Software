5cd9af03975007bc86cbdf2907b5cd682b982912
Martin Reyes <martinreyes@macOS.local>
2023-07-11 12:20:34 -0300

    Funciona. Lo que habiamos hecho + app.py del video

--- 4_Implementacion/backend/main/__init__.py
+++ 4_Implementacion/backend/main/__init__.py
@@ -1,27 +1,27 @@
-import os
-from flask import Flask
-from dotenv import load_dotenv
-from flask_restful import Api
-from flask_jwt_extended import JWTManager
+# import os
+# from flask import Flask
+# from dotenv import load_dotenv
+# from flask_restful import Api
+# from flask_jwt_extended import JWTManager
 
-api = Api()
-jwt = JWTManager()
+# api = Api()
+# jwt = JWTManager()
 
-def create_app():
-	app = Flask(__name__)
-	load_dotenv()
+# def create_app():
+# 	app = Flask(__name__)
+# 	load_dotenv()
 
-	# import main.resources as resources
-	# api.add_resource(resources.PoemaResource, '/poema/<poema_id>')
+# 	# import main.resources as resources
+# 	# api.add_resource(resources.PoemaResource, '/poema/<poema_id>')
 	
-	api.init_app(app)
+# 	api.init_app(app)
 	
-	app.config['PROPAGATE_EXCEPTIONS'] = True
-	app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-	app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
-	jwt.init_app(app)
+# 	app.config['PROPAGATE_EXCEPTIONS'] = True
+# 	app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+# 	app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
+# 	jwt.init_app(app)
 
-	from main.auth import routes
-	app.register_blueprint(routes.auth)
+# 	from main.auth import routes
+# 	app.register_blueprint(routes.auth)
 
-	return app
+# 	return app
